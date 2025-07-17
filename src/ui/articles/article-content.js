a69e2b8fab8c282e94c26cc69c13f4314054c7af
DavidMaksic <david.maksic88@gmail.com>
2025-07-17 18:42:59 +0200

    Fix

--- src/ui/articles/article-content.js
+++ src/ui/articles/article-content.js
@@ -23,7 +23,7 @@ function ArticleContent({ content, article }) {
       <motion.div
          className={`container text-text my-3 [&_:is(h1,h2,h3)]:font-headers ${
             article.language === 'English'
-               ? `text-2xl xl:text-[1.325rem] md:text-[1.6rem] [&_p]:leading-[1.49]! xs:[&_p]:leading-[1.42]! lg:[&_blockquote]:text-[1.5rem] md:[&_blockquote]:text-[1.75rem] lg:[&_blockquote>*]:leading-8.5 md:[&_blockquote>*]:leading-10 sm:[&_blockquote>*]:leading-9.5 ${crimsonText.className}`
+               ? `text-2xl xl:text-[1.325rem] md:text-[1.6rem] [&_p]:leading-[1.49]! xs:[&_p]:leading-[1.4]! lg:[&_blockquote]:text-[1.5rem] md:[&_blockquote]:text-[1.75rem] lg:[&_blockquote>*]:leading-8.5 md:[&_blockquote>*]:leading-10 sm:[&_blockquote>*]:leading-9.5 ${crimsonText.className}`
                : `text-[1.4rem] xl:text-[1.235rem] lg:text-[1.178rem] md:text-[1.4rem] xs:text-[1.55rem] [&_p]:leading-[1.65]! xl:[&_p]:leading-[1.7] lg:[&_p]:leading-[1.65]! xs:[&_p]:leading-[1.47]! [&_blockquote]:text-2xl! xl:[&_blockquote]:text-[1.32rem]! lg:[&_blockquote]:text-[1.26rem]! md:[&_blockquote]:text-[1.6rem]! [&_blockquote>*]:leading-[1.5]! ${gentium.className}`
          }`}
          initial={{ opacity: 0 }}
