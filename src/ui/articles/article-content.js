65b3640abd481dd40e2851b13f80b20f6df38322
DavidMaksic <david.maksic88@gmail.com>
2025-07-19 12:40:04 +0200

    Fix

--- src/ui/articles/article-content.js
+++ src/ui/articles/article-content.js
@@ -23,8 +23,8 @@ function ArticleContent({ content, article }) {
       <motion.div
          className={`container text-text my-3 [&_:is(h1,h2,h3)]:font-headers ${
             article.language === 'English'
-               ? `text-2xl xl:text-[1.325rem] md:text-[1.6rem] [&_p]:leading-[1.49]! xs:[&_p]:leading-[1.4]! lg:[&_blockquote>*]:text-[1.4rem] md:[&_blockquote>*]:text-[1.75rem] xl:[&_blockquote>*]:leading-7.5 lg:[&_blockquote>*]:leading-7.5 md:[&_blockquote>*]:leading-9.5 sm:[&_blockquote>*]:leading-9.5 xs:[&_blockquote>*]:leading-8 ${crimsonText.className}`
-               : `text-[1.4rem] xl:text-[1.235rem] lg:text-[1.178rem] md:text-[1.4rem] xs:text-[1.55rem] [&_p]:leading-[1.65]! xl:[&_p]:leading-[1.7] lg:[&_p]:leading-[1.65]! xs:[&_p]:leading-[1.47]! [&_blockquote>*]:text-2xl! xl:[&_blockquote>*]:text-[1.32rem]! lg:[&_blockquote>*]:text-[1.26rem]! md:[&_blockquote>*]:text-[1.6rem]! [&_blockquote>*]:leading-[1.5]! ${gentium.className}`
+               ? `text-2xl xl:text-[1.325rem] md:text-[1.6rem] [&_p]:leading-[1.49]! xs:[&_p]:leading-[1.4]! lg:[&_blockquote>*]:text-[1.4rem] md:[&_blockquote>*]:text-[1.75rem] xl:[&_blockquote>*]:leading-7.5 lg:[&_blockquote>*]:leading-7.5 md:[&_blockquote>*]:leading-9.5 sm:[&_blockquote>*]:leading-9.5 xs:[&_blockquote>*]:leading-7.5 ${crimsonText.className}`
+               : `text-[1.4rem] xl:text-[1.235rem] lg:text-[1.178rem] md:text-[1.4rem] xs:text-[1.55rem] [&_p]:leading-[1.62]! xl:[&_p]:leading-[1.7] lg:[&_p]:leading-[1.65]! xs:[&_p]:leading-[1.47]! [&_blockquote>*]:text-2xl! xl:[&_blockquote>*]:text-[1.32rem]! lg:[&_blockquote>*]:text-[1.26rem]! md:[&_blockquote>*]:text-[1.6rem]! [&_blockquote>*]:leading-[1.5]! ${gentium.className}`
          }`}
          initial={{ opacity: 0 }}
          animate={{ opacity: 1 }}
