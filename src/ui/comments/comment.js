1e77b37d332e3bdd0181b86a55acc05ebbf84428
DavidMaksic <david.maksic88@gmail.com>
2025-07-23 14:04:01 +0200

    Fix

--- src/ui/comments/comment.js
+++ src/ui/comments/comment.js
@@ -100,7 +100,7 @@ function Comment({
       <>
          <div
             id={`comment-${commentID}`}
-            className="flex flex-col gap-5 bg-secondary dark:bg-primary-200 xs:bg-primary-300/15 box-shadow rounded-3xl px-14 sm:px-12 xs:px-10 py-10 sm:py-8 xs:py-6 scroll-mt-28! transition duration-300"
+            className="flex flex-col gap-5 bg-secondary dark:bg-primary-200 xs:dark:bg-primary-300/15 box-shadow rounded-3xl px-14 sm:px-12 xs:px-10 py-10 sm:py-8 xs:py-6 scroll-mt-28! transition duration-300"
          >
             <div className="flex items-center justify-between">
                <div className="flex items-center gap-4">
@@ -140,7 +140,7 @@ function Comment({
 
             <div className="flex items-center gap-2 md:gap-2.5 mt-2 md:mt-3">
                <div
-                  className={`flex items-center gap-2 h-9 md:h-11 xs:h-10.5 w-fit rounded-xl px-3 py-1.5 bg-primary-300/20 dark:bg-primary-400/12 text-primary-500/80 hover:bg-primary-200/60 dark:hover:bg-primary-400/20 cursor-pointer transition-75 ${
+                  className={`flex items-center gap-2 h-9 md:h-11 xs:h-10.5 w-fit rounded-xl px-3 py-1.5 bg-primary-300/20 dark:bg-primary-400/12 xs:dark:bg-primary-300/20 text-primary-500/80 hover:bg-primary-200/60 dark:hover:bg-primary-400/20 cursor-pointer transition-75 ${
                      commentCount === 0 && 'gap-0!'
                   }`}
                   onClick={() => {
@@ -178,7 +178,7 @@ function Comment({
                </div>
 
                <div
-                  className="flex items-center gap-2 h-9 md:h-11 xs:h-10.5 w-fit rounded-xl px-3 md:px-4 py-1.5 bg-primary-300/20 dark:bg-primary-400/12 text-primary-500/80 hover:bg-primary-200/60 dark:hover:bg-primary-400/20 cursor-pointer transition-75"
+                  className="flex items-center gap-2 h-9 md:h-11 xs:h-10.5 w-fit rounded-xl px-3 md:px-4 py-1.5 bg-primary-300/20 dark:bg-primary-400/12 xs:dark:bg-primary-300/20 text-primary-500/80 hover:bg-primary-200/60 dark:hover:bg-primary-400/20 cursor-pointer transition-75"
                   onClick={() => {
                      if (!session) setIsOpen(true);
                      if (session) setReplyIsOpen((isOpen) => !isOpen);
