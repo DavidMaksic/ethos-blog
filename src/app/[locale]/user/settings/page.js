41f67be0ec71e74639e72d6b63e891eaafc6ed48
DavidMaksic <david.maksic88@gmail.com>
2025-07-23 15:58:17 +0200

    Fix

--- src/app/[locale]/user/settings/page.js
+++ src/app/[locale]/user/settings/page.js
@@ -23,7 +23,7 @@ async function Page() {
                   <label className="text-sm uppercase font-semibold text-primary-400 tracking-wider">
                      {t('username')}
                   </label>
-                  <span className="text-3xl xs:text-[2rem]">
+                  <span className="text-3xl xs:text-4xl">
                      {username ? username : session.user.name}
                   </span>
                </div>
