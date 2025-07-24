d7103dc9eab8e00e1f979cc3005546279fec04b2
DavidMaksic <david.maksic88@gmail.com>
2025-07-24 14:22:14 +0200

    Fix

--- src/ui/about-blocks.js
+++ src/ui/about-blocks.js
@@ -22,21 +22,19 @@ function AboutBlocks({ authors }) {
                <h1 className="text-4xl text-primary-600/75 font-bold">
                   {t('label')}
                </h1>
-               <p className="text-[1.35rem] leading-7 md:leading-8 md:text-2xl">
+               <p className="text-[1.35rem] leading-7.5 md:leading-8 md:text-2xl">
                   {t('description')}
                </p>
             </section>
 
-            <section
-               className={`space-y-1.5 col-span-4 md:col-span-7 sm:col-span-8 py-10 lg:py-8 md:pb-9 px-14 lg:px-12 bg-white dark:bg-primary-300/10 border border-quaternary dark:border-primary-300/15 rounded-2xl md:rounded-3xl box-shadow font-medium`}
-            >
+            <section className="space-y-1.5 col-span-4 md:col-span-7 sm:col-span-8 py-10 lg:py-8 md:pb-9 px-14 lg:px-12 bg-white dark:bg-primary-300/10 border border-quaternary dark:border-primary-300/15 rounded-2xl md:rounded-3xl box-shadow font-medium">
                <h2 className="text-4xl text-primary-600/75 font-bold">
                   {t('label-2')}
                </h2>
                <span className="text-primary-400 text-[1.4rem]">
                   {t('ipa')}
                </span>
-               <p className="text-[1.35rem] leading-7 md:leading-8 md:text-2xl mt-3">
+               <p className="text-[1.35rem] leading-7.5 md:leading-8 md:text-2xl mt-3">
                   {t.rich('etymology', {
                      em: (chunks) => <em>{chunks}</em>,
                   })}
