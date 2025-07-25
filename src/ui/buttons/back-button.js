a6755b8ed90c3acd5f53eff12c75b36a4afa08c8
DavidMaksic <david.maksic88@gmail.com>
2025-07-25 13:24:43 +0200

    Fix

--- src/ui/buttons/back-button.js
+++ src/ui/buttons/back-button.js
@@ -13,9 +13,7 @@ function BackButton() {
 
    return (
       <HiOutlineArrowUturnLeft
-         className="w-72 h-40 rounded-xl bg-[rgba(30,30,30,0.7)] backdrop-blur-md text-white flex items-center justify-center cursor-pointer transition-all duration-300
-                    hover:bg-gradient-to-b hover:from-[rgba(255,255,255,0.25)] hover:to-[rgba(255,255,255,0.05)]
-                    hover:bg-[rgba(30,30,30,0.7)] border border-white/20 shadow-lg"
+         className="size-10 xl:size-[2.4rem] backdrop-blur-md p-2 stroke-2 text-primary-400 group-hover:text-primary-500 dark:text-primary-400 dark:group-hover:text-primary-500 transition-color hover:bg-gradient-to-b hover:from-[rgba(255,255,255,0.25)] hover:to-[rgba(255,255,255,0.05)] hover:bg-[rgba(30,30,30,0.7)] border border-white/20"
          onClick={navigate}
       />
    );
