33edd4a0d696f617c67e5aee31cf3d2fb931921b
DavidMaksic <david.maksic88@gmail.com>
2025-07-31 17:33:31 +0200

    Fix

--- src/context/language-context.js
+++ src/context/language-context.js
@@ -8,7 +8,7 @@ import enFlag from '@/assets/en-flag.png';
 
 const LanguageContext = createContext();
 
-function LanguageProvider({ children }) {
+export function LanguageProvider({ children }) {
    const locale = useLocale();
    const [language, setLanguage] = useState({
       language: 'English',
@@ -16,14 +16,11 @@ function LanguageProvider({ children }) {
    });
 
    useEffect(() => {
-      if (!locale) return;
       if (locale === 'sr-cyrl') {
          setLanguage({ language: 'Српски', flag: srbFlag });
       } else {
          setLanguage({ language: 'English', flag: enFlag });
       }
-
-      document.cookie = `NEXT_LOCALE=${locale}; path=/; max-age=31536000`;
    }, [locale]);
 
    return (
@@ -33,10 +30,8 @@ function LanguageProvider({ children }) {
    );
 }
 
-function useLanguage() {
+export function useLanguage() {
    const context = useContext(LanguageContext);
    if (!context) throw new Error('useLanguage must be inside LanguageProvider');
    return context;
 }
-
-export { LanguageProvider, useLanguage };
