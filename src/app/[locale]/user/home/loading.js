c92b76a8c1fc83a0eb98bc0e7e0404fb638fd515
DavidMaksic <david.maksic88@gmail.com>
2025-08-06 14:26:46 +0200

    Fix

--- src/app/[locale]/user/home/loading.js
+++ src/app/[locale]/user/home/loading.js
@@ -12,7 +12,7 @@ function Loading() {
          transition={{ duration: 0.3 }}
       >
          <div className="space-y-4">
-            <div className="w-[460px] 2xl:w-[392px] lg:w-[342px] md:w-full h-[24.5rem] 2xl:h-[25.5rem] lg:h-[20.5rem] md:h-[310px] xs:h-[250px] 2xs:h-[217px] self-start rounded-3xl bg-primary-300/50 px-28 py-20" />
+            <div className="2k:w-[31rem] w-[460px] 2xl:w-[400px] lg:w-[342px] md:w-full h-[24.5rem] 2xl:h-[25.5rem] lg:h-[20.5rem] md:h-[310px] xs:h-[250px] 2xs:h-[217px] self-start rounded-3xl bg-primary-300/50 px-28 py-20" />
             <div className="w-42 md:w-full h-22 xs:h-23 self-start px-8 py-4 pb-5 rounded-3xl bg-primary-300/40" />
          </div>
 
