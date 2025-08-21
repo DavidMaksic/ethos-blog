3cc7e94c5fe94a78e7ab917431c8ed22304b6c1c
DavidMaksic <david.maksic88@gmail.com>
2025-08-21 16:20:49 +0200

    Fixed language image bug

--- src/ui/buttons/language-button.js
+++ src/ui/buttons/language-button.js
@@ -85,6 +85,7 @@ function LanguageButton() {
                            <Image
                               className="border border-primary-300 dark:border-primary-200 rounded-full group-hover:opacity-100 dark:group-hover:opacity-95 transition-[opacity]"
                               fill
+                              priority={true}
                               src={item.flag}
                               alt={item.lang}
                            />
