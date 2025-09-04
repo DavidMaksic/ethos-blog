c53bcb3713ac1bd622f40cc67c3e88bdea2854f1
DavidMaksic <david.maksic88@gmail.com>
2025-09-04 15:00:23 +0200

    Testing Open Graph images

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -106,7 +106,7 @@ export async function generateMetadata({ params }) {
          type: 'website',
          images: [
             {
-               url: 'https://qjbihfajkucvfxqkvtxk.supabase.co/storage/v1/object/public/misc/ethos-banner.png',
+               url: 'https://qjbihfajkucvfxqkvtxk.supabase.co/storage/v1/object/public/misc/ethos-banner-2.png',
                width: 136,
                height: 136,
                alt: isEnglish ? 'Ethos Blog' : 'Етос блог',
@@ -120,7 +120,7 @@ export async function generateMetadata({ params }) {
             ? 'Ethos blog features many authors from across the world, who write with great interest on various topics connected to the concept of ethos - culture, customs, values, ethics...'
             : 'За Eтос блог пишу аутори из целог света, са великим занимањем за разне теме које су повезане са идејом етоса - културом, обичајима, вредностима, етиком...',
          images: [
-            'https://qjbihfajkucvfxqkvtxk.supabase.co/storage/v1/object/public/misc/ethos-banner.png',
+            'https://qjbihfajkucvfxqkvtxk.supabase.co/storage/v1/object/public/misc/ethos-banner-2.png',
          ],
       },
    };
