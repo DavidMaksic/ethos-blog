6d50ecefaa378d5cb24e03841b6003f318c9c653
DavidMaksic <david.maksic88@gmail.com>
2025-09-06 16:44:39 +0200

    Test for manifest.json

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -141,7 +141,7 @@ export async function generateMetadata({ params }) {
             'https://qjbihfajkucvfxqkvtxk.supabase.co/storage/v1/object/public/misc/ethos-banner-3.png',
          ],
       },
-      manifest: `${WEBSITE_URL}/${locale}/manifest.json`,
+      manifest: `/${locale}/manifest.json`,
       other: {
          'script:ld+json': JSON.stringify(jsonLd).replace(/</g, '\\u003c'),
       },
