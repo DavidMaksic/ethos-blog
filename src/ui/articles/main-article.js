b004bfe79f0a98ba449e0b8aeca64b1a0fd608ae
DavidMaksic <david.maksic88@gmail.com>
2025-09-09 15:53:58 +0200

    Made links more descriptive

--- src/ui/articles/main-article.js
+++ src/ui/articles/main-article.js
@@ -18,10 +18,10 @@ function MainArticle({ article }) {
 
             <Link
                href={`/${article.slug}`}
-               aria-label={`Read article: ${article.title}`}
                prefetch
                className="self-start md:self-center mt-7 2xl:mt-5 md:mt-5 xs:mt-3 rounded-full bg-gradient-to-r from-accent-300/80 to-accent-600/70 hover:from-transparent hover:to-transparent border-2 border-transparent hover:border-accent/80 shadow-btn hover:shadow-none dark:shadow-none transition-[box-shadow,border,--tw-gradient-from,--tw-gradient-to] duration-300 bg-origin-border group"
             >
+               <span className="sr-only">{`Read more about ${article.title}`}</span>
                <span
                   className={`flex articles-center gap-5 text-4xl 2xl:text-[1.8rem] md:text-[2rem] sm:text-[1.8rem] xs:text-[1.9rem] pl-8 2xl:pl-5 md:pl-6 xs:pl-5 pr-9 2xl:pr-6 md:pr-6.5 xs:pr-6 py-3.5 2xl:py-3 lg:py-2.5 md:py-3.5 xs:py-2.5 text-white group-hover:text-accent group-hover:drop-shadow-xs dark:group-hover:text-accent/90 transition-[color] duration-300 cursor-pointer ${
                      locale === 'en' && 'font-logo'
