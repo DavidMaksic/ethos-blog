859c4d296aed595fa757554f4fef134d1fa8fac1
DavidMaksic <david.maksic88@gmail.com>
2025-09-10 18:21:08 +0200

    Testing image optimizations

--- src/ui/rich-text-renderer.js
+++ src/ui/rich-text-renderer.js
@@ -17,11 +17,11 @@ export default function RichTextRenderer({ html }) {
                   className={domNode.attribs.class}
                   src={domNode.attribs.src}
                   alt="Article image"
-                  width={4000}
-                  height={3000}
-                  quality={60}
+                  width={1000}
+                  height={400}
+                  quality={40}
                   priority={true}
-                  sizes="(max-width: 400px) 300px, (max-width: 630px) 600px, (max-width: 1020px) 1000px, (max-width: 2300px) 800px, 100vw"
+                  sizes="(max-width: 400px) 300px, (max-width: 630px) 600px, (max-width: 1020px) 1000px, (max-width: 2300px) 400px, 100vw"
                />
             );
          }
