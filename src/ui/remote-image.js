4bc36002785234ee51672ac948f909bd5d7273a3
DavidMaksic <david.maksic88@gmail.com>
2025-09-10 18:46:06 +0200

    Test

--- src/ui/remote-image.js
+++ src/ui/remote-image.js
@@ -10,7 +10,7 @@ function RemoteImage({ imageRef = null, imageUrl, styles, alt }) {
          alt={alt ? alt : 'Image'}
          priority={true}
          fetchPriority="high"
-         sizes="(max-width: 400px) 300px, (max-width: 630px) 600px, (max-width: 1020px) 1000px, (max-width: 2300px) 1200px, 100vw"
+         sizes="(max-width: 630px) 100vw, (max-width: 1020px) 1000px, (max-width: 2300px) 1200px, 100vw"
       />
    );
 }
