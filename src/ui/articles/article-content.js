e5ed1ddfa7ce842f5f96b395ec90b5fbe71a131a
DavidMaksic <david.maksic88@gmail.com>
2025-09-18 19:13:05 +0200

    Changed quote and image caption styles

--- src/ui/articles/article-content.js
+++ src/ui/articles/article-content.js
@@ -5,7 +5,7 @@ import RichTextRenderer from '@/src/ui/rich-text-renderer';
 const crimsonText = Crimson_Text({
    subsets: ['latin'],
    style: ['normal', 'italic'],
-   weight: ['400', '700'],
+   weight: ['400', '600', '700'],
    variable: '--font-crimsonText',
 });
 
@@ -24,7 +24,7 @@ function ArticleContent({ content, article }) {
             crimsonText.variable
          }  ${gentium.variable} ${
             article.language === 'English'
-               ? `text-2xl 2xl:text-[1.326rem] md:text-[1.6rem] [&_p]:leading-[1.49]! xs:[&_p]:leading-[1.35]! 4k:[&_blockquote>*]:text-[1.695rem]! 2k:[&_blockquote>*]:text-[1.65rem] [&_blockquote>*]:text-[1.65rem] 2xl:[&_blockquote>*]:text-[1.41rem] lg:[&_blockquote>*]:text-[1.438rem] md:[&_blockquote>*]:text-[1.75rem] xs:[&_blockquote>*]:text-[1.7rem] [&_blockquote>*]:leading-[1.23] 2xl:[&_blockquote>*]:leading-7.5 lg:[&_blockquote>*]:leading-7.5 md:[&_blockquote>*]:leading-9.5 sm:[&_blockquote>*]:leading-9.5 xs:[&_blockquote>*]:leading-[1.29] xs:[&_blockquote]:leading-[1.29] font-article`
+               ? `text-2xl 2xl:text-[1.326rem] md:text-[1.6rem] [&_p]:leading-[1.49]! xs:[&_p]:leading-[1.35]! [&_blockquote>*]:font-main [&_blockquote>*]:font-semibold 4k:[&_blockquote>*]:text-[1.695rem]! 2k:[&_blockquote>*]:text-[1.65rem] [&_blockquote>*]:text-[1.65rem] 2xl:[&_blockquote>*]:text-[1.41rem] lg:[&_blockquote>*]:text-[1.438rem] md:[&_blockquote>*]:text-[1.75rem] xs:[&_blockquote>*]:text-[1.7rem] [&_blockquote>*]:leading-[1.23] 2xl:[&_blockquote>*]:leading-7.5 lg:[&_blockquote>*]:leading-7.5 md:[&_blockquote>*]:leading-9.5 sm:[&_blockquote>*]:leading-9.5 xs:[&_blockquote>*]:leading-[1.29] xs:[&_blockquote]:leading-[1.29] font-article`
                : `text-[1.4rem] 2xl:text-[1.235rem] lg:text-[1.178rem] md:text-[1.4rem] xs:text-[1.55rem] [&_p]:leading-[1.6]! 2xl:[&_p]:leading-[1.7] lg:[&_p]:leading-[1.65]! xs:[&_p]:leading-[1.44]! [&_blockquote>*]:text-2xl! 2xl:[&_blockquote>*]:text-[1.32rem]! lg:[&_blockquote>*]:text-[1.26rem]! md:[&_blockquote>*]:text-[1.6rem]! [&_blockquote>*]:leading-[1.5] xs:[&_blockquote>*]:leading-[1.4] font-article-sr`
          }`}
       >
