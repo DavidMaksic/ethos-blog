c8c066ceb23e11296eb174eccd0555de56903b58
DavidMaksic <david.maksic88@gmail.com>
2025-10-02 12:51:20 +0200

    Testing optimizations

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,30 +1,14 @@
 import {
+   getArticles,
    getMainArticles,
    getCategories,
-   getArticles,
    getAuthors,
 } from '@/src/lib/data-service';
 import { getTranslations } from 'next-intl/server';
-import dynamic from 'next/dynamic';
 
-const FeaturedArticles = dynamic(
-   () => import('@/src/ui/articles/featured-articles'),
-   {
-      ssr: true,
-      loading: () => <div>Loading...</div>,
-   }
-);
-const LatestArticles = dynamic(
-   () => import('@/src/ui/articles/latest-articles'),
-   {
-      ssr: true,
-      loading: () => <div>Loading...</div>,
-   }
-);
-const MainArticles = dynamic(() => import('@/src/ui/articles/main-articles'), {
-   ssr: true,
-   loading: () => <div>Loading...</div>,
-});
+import FeaturedArticles from '@/src/ui/articles/featured-articles';
+import LatestArticles from '@/src/ui/articles/latest-articles';
+import MainArticles from '@/src/ui/articles/main-articles';
 
 export default async function Home() {
    const [articles, categories, mainArticles, authors, t] = await Promise.all([
