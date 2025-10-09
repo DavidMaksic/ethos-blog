cbd407930834e75d5d05bd04d9c0eb11a1b5515c
DavidMaksic <david.maksic88@gmail.com>
2025-10-09 12:32:41 +0200

    Testing SEO optimization

--- src/ui/articles/article-item.js
+++ src/ui/articles/article-item.js
@@ -36,7 +36,7 @@ function ArticleItem({ article, style }) {
          <Link
             href={`/${article.slug}`}
             prefetch
-            className={`relative grid grid-cols-[0.4fr_1fr] lg:grid-cols-[0.3fr_1fr] sm:grid-cols-1 rounded-2xl group cursor-pointer overflow-hidden border border-quaternary dark:border-primary-300/10 bg-white ${style} hover:translate-x-1.5 transition-[translate] duration-200 select-none box-shadow sm:h-fit`}
+            className={`relative grid grid-cols-[0.4fr_1fr] lg:grid-cols-[0.3fr_1fr] sm:grid-cols-1 rounded-2xl group cursor-pointer overflow-hidden border border-quaternary dark:border-primary-300/10 bg-white ${style} hover:translate-x-1.5 transition-[translate] duration-200 box-shadow sm:h-fit`}
          >
             <div className="relative h-50 2xl:h-54 lg:h-52 md:h-61 sm:hidden">
                <RemoteImage
