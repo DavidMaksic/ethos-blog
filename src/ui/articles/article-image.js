0bb0c8372e9ec3e757c1bf4b5e37c6448761bc1a
DavidMaksic <david.maksic88@gmail.com>
2025-12-17 13:13:01 +0100

    Fixes for image optimizations

--- src/ui/articles/article-image.js
+++ src/ui/articles/article-image.js
@@ -7,10 +7,11 @@ import Image from 'next/image';
 
 async function ArticleImage({ article, author, date, children }) {
    const locale = await getLocale();
+   const { image, categories } = article;
 
    return (
       <div className="flex flex-col border bg-white/50 dark:bg-primary-300/5 border-primary-300/70 dark:border-primary-300/15 rounded-3xl mt-3 shadow-article dark:shadow-menu-dark transition-bg_border">
-         <MainImage article={article} />
+         <MainImage url={image} />
 
          <div className="flex items-center justify-between gap-6 px-6 py-4 2xl:py-3 font-title">
             <div className="flex gap-4 2xl:gap-3.5 items-center">
@@ -48,7 +49,7 @@ async function ArticleImage({ article, author, date, children }) {
 
                <div className="pointer-events-none">
                   <Category
-                     category={article.categories}
+                     category={categories}
                      customStyles="text-xl! md:text-[1.4rem]! sm:text-[1.2rem]! !font-bold pb-[8px]! sm:pb-[6px]!"
                   />
                </div>
--- src/ui/main-image.js
+++ src/ui/main-image.js
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 
-function MainImage({ article }) {
+function MainImage({ url }) {
    return (
       <div className="container relative h-[24rem] 2xl:h-[21rem] sm:h-[17rem]">
          <Image
-            className="transition-opacity duration-700 ease-in-out main-image rounded-3xl object-cover opacity-95 dark:opacity-80"
-            src={article.image}
+            className="transition-opacity duration-700 ease-in-out main-image rounded-3xl object-cover opacity-90 dark:opacity-75"
+            src={url}
             alt="Main image"
             fetchPriority="high"
             unoptimized
