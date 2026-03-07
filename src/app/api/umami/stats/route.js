fbf8ca791d647ac48dc9d2d075b4e4d9e93c41e3
DavidMaksic <david.maksic88@gmail.com>
2026-03-07 14:58:10 +0100

    Added another api route for fetching umami data

---
+++ src/app/api/umami/stats/route.js
@@ -0,0 +1,28 @@
+export async function GET(request) {
+   const { searchParams } = new URL(request.url);
+
+   const res = await fetch(
+      `https://api.umami.is/v1/websites/${process.env.UMAMI_WEBSITE_ID}/stats?${searchParams}`,
+      { headers: { 'x-umami-api-key': process.env.UMAMI_API_KEY } },
+   );
+
+   const data = await res.json();
+
+   return Response.json(data, {
+      headers: {
+         'Access-Control-Allow-Origin': process.env.CMS_URL,
+         'Access-Control-Allow-Methods': 'GET, OPTIONS',
+         'Access-Control-Allow-Headers': 'Content-Type',
+      },
+   });
+}
+
+export async function OPTIONS() {
+   return new Response(null, {
+      headers: {
+         'Access-Control-Allow-Origin': process.env.CMS_URL,
+         'Access-Control-Allow-Methods': 'GET, OPTIONS',
+         'Access-Control-Allow-Headers': 'Content-Type',
+      },
+   });
+}
