2c152ffef441592789edc28a1b96d57b8cd93d1f
DavidMaksic <david.maksic88@gmail.com>
2026-03-24 13:42:03 +0100

    Testing

--- src/ui/email/article-template.js
+++ src/ui/email/article-template.js
@@ -75,7 +75,7 @@ export default function ArticleTemplate({
                      span { font-size: 10px !important; }
 
                      .outer { padding: 0px 0px !important; }
-                     .container { border-color: #fff !important; border-width: 0px !important; }
+                     .container { padding: 10px 1px !important; border-color: #fff !important; border-width: 0px !important; }
                      .article-image { height: 200px !important; max-width: 800px !important; }
                   }
                `}
